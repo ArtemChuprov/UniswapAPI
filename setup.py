b35031911a31e4f629bf33f172b1ac7758955063
ArtemChuprov <artyomchuprow@yandex.ru>
2024-03-20 19:47:14 +0300

    zero commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="DEX_API",
+    version="0.0.1",
+    author="Blockers",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Ubuntu",
+    ],
+    packages=["ClientTools"],
+    install_requires=["scikit-learn"],
+    python_requires=">=3.9",
+)
